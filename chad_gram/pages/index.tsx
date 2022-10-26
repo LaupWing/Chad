46b81e8cc1529be04c6e21b9e0f8320e67775e59
LaupWing <loc_strict@hotmail.com>
2022-10-26 20:13:18 +0200

    udpate

--- chad_gram/pages/index.tsx
+++ chad_gram/pages/index.tsx
@@ -1,19 +1,24 @@
 import type { NextPage } from "next"
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import Profile from "../components/Profile"
 import { createRandomUser } from "../faker"
 
 const Home: NextPage = () => {
-   const test = createRandomUser()
+   const [users, setUsers] = useState<any>([])
+   useEffect(()=>{
+      setUsers([...new Array(2)].map(_=>createRandomUser()))
+   },[])
+
    
    return (
       <div className="flex w-screen h-screen flex-col bg-gray1">
          <Header/>
          <div className="w-full px-0.5">
             <div className="w-full border-gray2 border p-2 rounded shadow flex">
-               <Profile
-                  user={test}
-               />
+               {users.map((user:any) =>(<Profile
+                  user={user}
+               />))}
             </div>
          </div>
       </div>
