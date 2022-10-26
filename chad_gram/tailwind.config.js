15f8fbc332d5dedb13b36af485a6ed7d9eaeb490
LaupWing <loc_strict@hotmail.com>
2022-10-26 17:21:34 +0200

    grayscale

--- chad_gram/tailwind.config.js
+++ chad_gram/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
       "./components/**/*.{js,ts,jsx,tsx}"
    ],
    theme: {
-      extend: {},
+      extend: {
+         colors:{
+            gray1: "#c1c5ce",
+            gray2: "#a7adbb",
+            gray3: "#65737e",
+            gray4: "#4f5b67",
+            gray5: "#343c46",
+         }
+      },
    },
    plugins: [],
 }
