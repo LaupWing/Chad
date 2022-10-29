b13e4e70627d4e2460e95db4df0e21490289f0d6
LaupWing <loc_strict@hotmail.com>
2022-10-29 17:09:10 +0200

    footer finished

--- chad_gram/components/Footer.tsx
+++ chad_gram/components/Footer.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { AiFillCustomerService, AiFillPlaySquare, AiOutlineHome, AiOutlineSearch } from "react-icons/ai"
+import { AiFillCustomerService, AiOutlineCompass, AiOutlineHome, AiOutlinePlusSquare, AiOutlineSend } from "react-icons/ai"
+import { createRandomUser } from "../faker"
 
 const Footer = () => {
+   const user = createRandomUser()
    return (
-      <footer className="bottom-0 sticky bg-gray1 text-gray5 flex justify-between p-4 border-t border-gray2">
-         <AiOutlineHome size={35}/>
-         <AiOutlineSearch size={35}/>
-         <AiFillPlaySquare size={35}/>
-         <AiFillCustomerService size={35}/>
+      <footer className="bottom-0 sticky bg-gray1 text-gray5 flex justify-between p-4 border-t border-gray2 items-center">
+         <AiOutlineHome className="w-1/5" size={30}/>
+         <AiOutlineCompass className="w-1/5" size={30}/>
+         <AiOutlinePlusSquare className="w-1/5" size={30}/>
+         <AiOutlineSend 
+            size={30} 
+            className="transform mb-2.5 w-1/5"
+            style={{
+               transform: "rotate(-30deg)"
+            }}
+         />
+         <div className="w-1/5 flex items-center justify-center">
+            <div className="flex flex-col p-0.5 rounded-full border-2 border-gray-500">
+               <img 
+                  src={user.avatar} 
+                  alt={user.username} 
+                  className="rounded-full w-7"
+               />
+            </div>
+         </div>
       </footer>
    )
 }
